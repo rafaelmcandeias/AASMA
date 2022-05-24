e81c6257d213f80ddc051ed6a8f5414aa9bb5b0f
VascoPiussa <vpiussa@localhost.localdomain>
2022-05-24 14:53:56 +0100

    added tennis game v1.0

---
+++ Tennis Game/pygame-tennis-master/Pygame/images.py
@@ -0,0 +1,17 @@
+import pygame
+
+#Robert's images
+robert = pygame.image.load("robert_imgs/Robert_tennis.png")
+robert_forehand = pygame.image.load("robert_imgs/Robert_tennis2 (1).png")
+robert_backhand = pygame.image.load("robert_imgs/Robert_tennis2_backhand.png")
+robert_forehand_volley = pygame.image.load("robert_imgs/Robert_tennis2 (1).png")
+robert_backhand_volley = pygame.image.load("robert_imgs/Robert_tennis2_backhand.png")
+robert_serve = pygame.image.load("robert_imgs/Robert_tennisserve-1.png.png")
+
+#Camden's images
+camden = pygame.image.load("camden_imgs/camden_tennis_front.png")
+camden_forehand = pygame.image.load("camden_imgs/camden_front_forehand.png")
+camden_backhand = pygame.image.load("camden_imgs/camden_front_backhand-1.png.png")
+camden_forehand_volley = pygame.image.load("camden_imgs/camden_front_forehand.png")
+camden_backhand_volley = pygame.image.load("camden_imgs/camden_front_backhand-1.png.png")
+camden_serve = pygame.image.load("camden_imgs/camden_tennis_serve-1.png.png")
